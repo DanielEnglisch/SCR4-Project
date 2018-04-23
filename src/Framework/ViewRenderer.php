e52b021bbabcd905223f8065d9509843540cc88b
Daniel Englisch <s1610307056@students.fh-hagenberg.at>
2018-04-23 15:35:36 +0200

    Added ViewRendere actionForm

--- src/Framework/ViewRenderer.php
+++ src/Framework/ViewRenderer.php
@@ -21,4 +21,25 @@ final class ViewRenderer{
             self::htmlOut($content);
         echo("</a>");
     }
+
+    
+    private static function beginActionForm($action, $controller, $params = null, $method = "GET", $cssClass = null){
+        $cc = $cssClass !== null ? " class=\"$cssClass\"" : '';
+        $c = MVC::PARAM_CONTROLLER;
+        $a = MVC::PARAM_ACTION;
+        $form = "
+            <form method=\"$method\" action=\"?\"$cc>
+            <input type=\"hidden\" name=\"$c\" value=\"$controller\"/>
+            <input type=\"hidden\" name=\"$a\" value=\"$action\"/>";
+        echo($form);
+        if(is_array($params)){
+            foreach($params as $key => $value){
+                echo("<input type=\"hidden\" name=\"$key\" value=\"$value\"/>");
+            }
+        }
+    }
+
+    private static function endActionForm(){
+        echo("</form>");
+    }
 }
