b708f86e9ca444c820954316ac70af496e2a8447
Daniel Englisch <s1610307056@students.fh-hagenberg.at>
2018-05-15 13:17:37 +0200

    Added more mockup data

--- src/BusinessLogic/DataLayer.php
+++ src/BusinessLogic/DataLayer.php
@@ -4,4 +4,8 @@ namespace BusinessLogic;
 
 interface DataLayer{
     public function getProducts();
+    public function getCategories();
+    public function getRatingsForProduct($product_id);
+    public function getUser($username);
+    public function getUserForUsernameAndPassword($username, $password);
 }
--- src/BusinessLogic/MockDataLayer.php
+++ src/BusinessLogic/MockDataLayer.php
@@ -4,25 +4,43 @@ namespace BusinessLogic;
 use \Models\Product;
 use \Models\Category;
 use \Models\User;
-use \Models\Raiting;
+use \Models\Rating;
 
 
 class MockDataLayer implements DataLayer {
+
   	private $categories;
 	private $products;
 	private $users;
-	private $raitings;
+	private $ratings;
 
 	public function __construct() {
 
+	    $this->categories = array(
+	        1 => new Category("Office"),
+            2 => new Category("Car"),
+            3 => new Category("Tools"),
+        );
+
+	    $this->users = array(
+	        1 => new User("Daniel"),
+            2 => new User("Andi"),
+            3 => new User("Maike"),
+        );
+
 		$this->products = array(
-			1 => new Product(1, "DanielEnglisch", "Reifen", "Mojo", "4", "2.3", "car"),
-			2 => new Product(2, "Alex", "Buntstifte", "Fabacastel", "1", "3", "office"),
-			3 => new Product(3, "MasterX", "Gebrauchte Taschentücher", "Tempo", "3", "5", "office"),
-			4 => new Product(4, "Xer0", "Büroklammern", "Pagro", "4", "1.4", "office"),
+			1 => new Product(1, "Daniel", "Reifen", "Mojo", "4", "2.3", "Car"),
+			2 => new Product(2, "Daniel", "Buntstifte", "Fabacastel", "1", "3", "Office"),
+			3 => new Product(3, "Andi", "Gebrauchte Taschentücher", "Tempo", "3", "5", "Office"),
+			4 => new Product(4, "Maike", "Büroklammern", "Pagro", "4", "1.4", "Tools"),
 		);
 
-		
+        $this->ratings = array(
+            1 => new Rating(1,1,"Andi","14.12.1996",2,"Good product!"),
+            2 => new Rating(2,1,"Maike","14.12.1996",3,"Ok product!"),
+            3 => new Rating(3,3,"Daniel","14.12.1996",1,"Flawless product!"),
+            4 => new Rating(4,4,"Daniel","14.12.1996",5,"Bad product!"),
+        );
 
 	}
 
@@ -30,5 +48,41 @@ class MockDataLayer implements DataLayer {
 		return $this->products;
 	}
 
-	
+	public function getCategories(){
+		return $this->categories;
+	}
+    public function getRatingsForProduct($product_id){
+		$res = array();
+
+		foreach ($this->ratings as $id => $rating){
+            if($rating->getId() == $product_id)
+                $res[] = $rating;
+        }
+
+		return $res;
+	}
+
+
+    public function getUser($username)
+    {
+        $res = array();
+
+        foreach ($this->users as $id => $user){
+            if($user->getId() == $username)
+                $res[] = $user;
+        }
+
+        return $res;
+    }
+
+    public function getUserForUsernameAndPassword($username, $password)
+    {
+        foreach ($this->users as $id => $user){
+            if($user->getUsername() == $username)
+                if($username == $password)
+                    return $user;
+        }
+
+        return null;
+    }
 }
